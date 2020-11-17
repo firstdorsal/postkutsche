68110a989b12d7f1468575e1cc76fbf1130a9872
Paul Colin Hennig <xl9jthv_7bvgakv9o9wg0jabn2ylm91xxrzzgt0e@y.gy>
2020-11-17 20:44:49 +0100

    module working

--- index.js
+++ index.js
@@ -1,177 +1,265 @@
 "use strict";
 
+const exec = require(`util`).promisify(require(`child_process`).exec);
+const secondLevelRegex = new RegExp(/[A-Z-a-z0-9]{1,63}\.[A-Z-a-z0-9]{1,63}$/);
+
 const {
-    resolve
-} = require(`path`);
-const util = require(`util`);
-const exec = util.promisify(require(`child_process`).exec);
+    MailcowApiClient
+} = require("mailcow-api")
+const {
+    PowerdnsClient
+} = require('@firstdorsal/powerdns-api');
 
-require('dotenv').config();
-const MAILSERVER_DOMAIN = process.env.MAILSERVER_DOMAIN
-const USE_LEGACY = process.env.USE_LEGACY
-const MAILSERVER_IP6 = process.env.MAILSERVER_IP6
-const MAILSERVER_IP4 = process.env.MAILSERVER_IP4
-const MAIL_DOMAIN = process.env.MAIL_DOMAIN
-const MAILSERVER_IP = USE_LEGACY ? MAILSERVER_IP4 : MAILSERVER_IP6;
+module.exports.Postkutsche = class {
+    constructor(info) {
+        this.mcc = new MailcowApiClient(info.mailcow.url, info.mailcow.apikey);
+        this.pdns = new PowerdnsClient(info.powerdns.url, info.powerdns.apikey);
+    }
 
-
-
-const tlsaQuestions = [{
-    name: `smtp over starttls`,
-    port: 25,
-    starttls: {
-        type: `smtp`
-    }
-}, {
-    name: `web over tls`,
-    port: 443,
-    starttls: false
-}, {
-    name: `pop3 over starttls`,
-    port: 110,
-    starttls: {
-        type: `pop3`
-    }
-}, {
-    name: `imap over starttls`,
-    port: 143,
-    starttls: {
-        type: `imap`
-    }
-}, {
-    name: `smtps over tls`,
-    port: 465,
-    starttls: false
-}, {
-    name: `submission over starttls`,
-    port: 587,
-    starttls: {
-        type: `smtp`
-    }
-}, {
-    name: `imaps over tls`,
-    port: 993,
-    starttls: false
-}, {
-    name: `pop3s over tls`,
-    port: 995,
-    starttls: false
-}, {
-    name: `sieve over starttls`,
-    port: 4190,
-    starttls: {
-        type: `sieve`
-    }
-}];
-
-const askTLSA = async (questions) => {
-    const answers = await Promise.all(questions.map((e) => {
-        e.answer = exec(`
+    getTLSA = async (info) => {
+        const questions = [{
+            name: `smtp over starttls`,
+            port: 25,
+            starttls: {
+                type: `smtp`
+            }
+        }, {
+            name: `web over tls`,
+            port: 443,
+            starttls: false
+        }, {
+            name: `pop3 over starttls`,
+            port: 110,
+            starttls: {
+                type: `pop3`
+            }
+        }, {
+            name: `imap over starttls`,
+            port: 143,
+            starttls: {
+                type: `imap`
+            }
+        }, {
+            name: `smtps over tls`,
+            port: 465,
+            starttls: false
+        }, {
+            name: `submission over starttls`,
+            port: 587,
+            starttls: {
+                type: `smtp`
+            }
+        }, {
+            name: `imaps over tls`,
+            port: 993,
+            starttls: false
+        }, {
+            name: `pop3s over tls`,
+            port: 995,
+            starttls: false
+        }, {
+            name: `sieve over starttls`,
+            port: 4190,
+            starttls: {
+                type: `sieve`
+            }
+        }];
+        const answers = await Promise.all(questions.map((e) => {
+            e.answer = exec(`
         echo | 
-        openssl s_client -servername ${MAILSERVER_DOMAIN} -connect ${MAILSERVER_IP}:${e.port} ${e.starttls?`-starttls `+e.starttls.type:``} 2>/dev/null | 
+        openssl s_client -servername ${info.mailServerHostname} -connect ${info.mailServerIp?info.mailServerIp:info.mailServerLegacyIp}:${e.port} ${e.starttls?`-starttls `+e.starttls.type:``} 2>/dev/null | 
         openssl x509 -pubkey -noout | 
         openssl pkey -pubin -outform DER | 
         openssl sha256 |
         sed 's/(stdin)= //g'`)
-        return e.answer;
-    }));
-    questions.map((e, i) => {
-        delete e.answer;
-        const sha256 = answers[i].stdout.replace(`\n`, ``);
-        e.answer = {
-            sha256
-        };
-        e.dns = {
-            fullName: `_${e.port}._tcp.${MAILSERVER_DOMAIN}`,
-            name: `_${e.port}._tcp`,
-            type: `tlsa`,
-            value: `3 1 1 ${sha256}`
-        }
-    });
-    return questions;
-}
+            return e.answer;
+        }));
+        questions.map((e, i) => {
+            delete e.answer;
+            const sha256 = answers[i].stdout.replace(`\n`, ``);
+            e.answer = {
+                sha256
+            };
+            e.dns = {
+                name: `_${e.port}._tcp.${info.mailServerHostname}.`,
+                type: `TLSA`,
+                content: [`3 1 1 ${sha256}`]
+            }
+        });
+        return questions;
+    }
 
-const generateMailRecords = (mailServerDomain, mailDomain, spfSettings = {
-    spfQualifier: 'FAIL',
-    spfMechanism: 'MX'
-}) => {
-
-    const SPF_QUALIFIERS = {
-        PASS: `+`,
-        NEUTRAL: `?`,
-        SOFTFAIL: `~`,
-        FAIL: `-`
-    };
-    const SPF_MECHANISMS = [`ALL`, `A`, `IP4`, `IP6`, `MX`, `PTR`, `EXISTS`, `INCLUDE`]
-    return [{
-            reverseDns: {
-                fullName: MAILSERVER_IP6.replace('[', '').replace(']', ''),
-                type: `PTR`,
-                value: mailServerDomain
-            }
-        }, {
-            reverseDns: {
-                //fullName: MAILSERVER_IP4.replace(/(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})/gm, `$4.$3.$2.$1.in-addr.arpa`),
-                fullName: MAILSERVER_IP4,
-                type: `PTR`,
-                value: mailServerDomain
-            }
-        }, {
-            dns: {
-                fullName: mailServerDomain,
-                name: `@`,
-                type: `AAAA`,
-                value: MAILSERVER_IP6.replace('[', '').replace(']', '')
-            }
-        }, {
-            dns: {
-                fullName: mailServerDomain,
-                name: `@`,
-                type: `A`,
-                value: MAILSERVER_IP4
-            }
-        }, {
-            dns: {
-                fullName: mailDomain,
-                name: `@`,
-                type: `MX`,
-                value: mailServerDomain
-            }
-        }, {
-            dns: {
-                fullName: `autodiscover.${mailDomain}`,
-                name: `autodiscover`,
-                type: `CNAME`,
-                value: mailServerDomain
-            }
-        }, {
-            dns: {
-                fullName: `autoconfig.${mailDomain}`,
-                name: `autoconfig`,
-                type: `CNAME`,
-                value: mailServerDomain
-            }
-        },
-        {
-            dns: {
-                fullName: `_autodiscover._tcp.${mailDomain}`,
-                name: `_autodiscover._tcp`,
-                type: `SRV`,
-                value: `${mailServerDomain} 443`
-            }
-        },
-        {
-            dns: {
-                fullName: mailDomain,
-                name: `@`,
+    genMailDomainRecords = (info) => {
+        return [{
+                name: `${info.mailDomain}.`,
+                type: 'MX',
+                content: [`10 ${info.mailServerHostname}.`]
+            }, {
+                name: `autodiscover.${info.mailDomain}.`,
+                type: 'CNAME',
+                content: [`${info.mailServerHostname}.`]
+            }, {
+                name: `_autodiscover._tcp.${info.mailDomain}.`,
+                type: 'SRV',
+                content: [`1 1 443 ${info.mailServerHostname}.`]
+            }, {
+                name: `autoconfig.${info.mailDomain}.`,
+                type: 'CNAME',
+                content: [`${info.mailServerHostname}.`]
+            }, {
+                name: `${info.mailDomain}.`,
                 type: `TXT`,
-                value: `v=spf1 ${spfSettings.spfMechanism} ${SPF_QUALIFIERS[spfSettings.spfQualifier]}all`
+                content: [`"v=spf1 MX -all"`]
+            }, {
+                name: `_dmarc.${info.mailDomain}.`,
+                type: `TXT`,
+                content: [`"v=DMARC1;p=reject;sp=reject;rua=mailto:${info.postmasterEmail};ruf=mailto:${info.postmasterEmail};adkim=s;aspf=s;"`]
+            }, {
+                name: `_imap._tcp.${info.mailDomain}.`,
+                type: 'SRV',
+                content: [`0 1 143 ${info.mailServerHostname}.`]
+            },
+            {
+                name: `_imaps._tcp.${info.mailDomain}.`,
+                type: 'SRV',
+                content: [`0 1 993 ${info.mailServerHostname}.`]
+            },
+            {
+                name: `_pop3._tcp.${info.mailDomain}.`,
+                type: 'SRV',
+                content: [`0 1 110 ${info.mailServerHostname}.`]
+            },
+            {
+                name: `_pop3s._tcp.${info.mailDomain}.`,
+                type: 'SRV',
+                content: [`0 1 995 ${info.mailServerHostname}.`]
+            },
+            {
+                name: `_submission._tcp.${info.mailDomain}.`,
+                type: 'SRV',
+                content: [`0 1 587 ${info.mailServerHostname}.`]
+            },
+            {
+                name: `_smtps._tcp.${info.mailDomain}.`,
+                type: 'SRV',
+                content: [`0 1 465 ${info.mailServerHostname}.`]
+            },
+            {
+                name: `_sieve._tcp.${info.mailDomain}.`,
+                type: 'SRV',
+                content: [`0 1 4190 ${info.mailServerHostname}.`]
+            },
+            {
+                name: `_carddavs._tcp.${info.mailDomain}.`,
+                type: 'SRV',
+                content: [`0 1 443 ${info.mailServerHostname}.`]
+            },
+            {
+                name: `_caldavs._tcp.${info.mailDomain}.`,
+                type: 'SRV',
+                content: [`0 1 443 ${info.mailServerHostname}.`]
+            },
+            {
+                name: `_carddavs._tcp.${info.mailDomain}.`,
+                type: 'TXT',
+                content: [`"path=/SOGo/dav/"`]
+            },
+            {
+                name: `_caldavs._tcp.${info.mailDomain}.`,
+                type: 'TXT',
+                content: [`"path=/SOGo/dav/"`]
             }
+        ]
+    }
+
+    addMailDomain = async (info) => {
+        info.domain = info.mailDomain;
+        const add = await Promise.all([this.pdns.createAndSetupZone(info), this.mcc.addDomain(info.mailDomain), this.mcc.addAndGetDKIM(info.mailDomain)]);
+        const records = this.genMailDomainRecords(info);
+        records.push({
+            name: `dkim._domainkey.${info.mailDomain}.`,
+            type: 'TXT',
+            content: [`"${add[2].dkim_txt}"`]
+        });
+        if (info.mailDomain !== info.dmarcMail.match(secondLevelRegex)[0]) {
+            this.pdns.setHomogeneousRecords([{
+                name: `${info.mailDomain}._report._dmarc.${info.dmarcMail.match(secondLevelRegex)[0]}`,
+                type: 'TXT',
+                content: [`"v=DMARC1"`]
+            }])
         }
 
-    ]
 
+        await Promise.all([
+            this.pdns.setHomogeneousRecords(records),
+            this.mcc.addMailbox({
+                domain: info.mailDomain,
+                name: info.defaultMailbox.name,
+                local_part: info.defaultMailbox.local_part
+            })
+        ]);
+    }
+
+
+
+    cleanupAddMailDomain = async (info) => {
+        await this.mcc.deleteMailbox(`${info.defaultMailbox.local_part}@${info.mailDomain}`)
+
+        await Promise.all([
+            this.mcc.deleteDomain(info.mailDomain),
+            this.mcc.deleteDKIM(info.mailDomain),
+            this.pdns.deleteZone(info.mailDomain),
+        ]);
+
+        if (info.mailDomain !== info.dmarcMail.match(secondLevelRegex)[0]) {
+            this.pdns.deleteRecords([{
+                name: `${info.mailDomain}._report._dmarc.${info.dmarcMail.match(secondLevelRegex)[0]}`,
+                type: 'TXT'
+            }])
+        }
+
+    }
+
+    addMailServerDnsRecords = async (info) => {
+        info.domain = info.mailServerHostname
+        await this.pdns.createAndSetupZone(info).catch(e => {
+            console.log(e);
+        });
+        const records = (await this.getTLSA(info)).map((e) => {
+            return e.dns
+        });
+        if (info.mailServerLegacyIp) {
+            records.push({
+                name: info.mailServerHostname,
+                type: 'A',
+                content: [info.mailServerLegacyIp]
+            });
+        }
+        if (info.mailServerIp) {
+            records.push({
+                name: info.mailServerHostname,
+                type: 'AAAA',
+                content: [info.mailServerIp]
+            });
+        }
+        if (info.addLetsEncryptCAA) {
+            const content = [`0 issue "digicert.com"`, `0 issue "letsencrypt.org"`];
+            info.caaReportMail ? content.push(`0 iodef "mailto:${info.caaReportMail}"`) : '';
+            records.push({
+                name: info.mailServerHostname.match(secondLevelRegex)[0],
+                type: 'CAA',
+                content: content
+            });
+        }
+        this.pdns.setHomogeneousRecords(records).catch(e => {
+            console.log(e);
+        });
+    }
+
+
+
+
+    cleanupAddMailServer = async (info) => {
+        await this.pdns.deleteZone(info.mailServerHostname)
+    }
 }
-
-//askTLSA(tlsaQuestions).then(res => console.log(res));
-console.log(generateMailRecords(MAILSERVER_DOMAIN, MAIL_DOMAIN));
