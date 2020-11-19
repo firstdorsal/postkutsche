1762b551be0221f49ec1b537f40b0949e4a6b724
Paul Colin Hennig <xl9jthv_7bvgakv9o9wg0jabn2ylm91xxrzzgt0e@y.gy>
2020-11-19 16:19:30 +0100

    add doc and ci

--- README_basic.md
+++ README_basic.md
@@ -1,6 +1,58 @@
+[![npm](https://ico.y.gy/npm/dm/postkutsche?style=flat-square&logo=npm)](https://www.npmjs.com/package/postkutsche)
+[![NPM](https://ico.y.gy/npm/l/postkutsche?style=flat-square)](https://www.npmjs.com/package/postkutsche)
+[![Snyk Vulnerabilities for npm package](https://ico.y.gy/snyk/vulnerabilities/npm/postkutsche?style=flat-square&logo=snyk)](https://snyk.io/test/npm/postkutsche)
+[![Website](https://ico.y.gy/website?down_color=red&down_message=offline&label=documentation&up_color=success&up_message=online&url=https%3A%2F%2Fdoc.y.gy%2Fpostkutsche&style=flat-square)](https://doc.y.gy/postkutsche/)
+[![Website](https://ico.y.gy/website?down_color=red&down_message=offline&label=repository&up_color=success&up_message=online&url=https%3A%2F%2Fgit.y.gy%2Ffirstdorsal%2Fpostkutsche&style=flat-square&logo=gitlab)](https://git.y.gy/firstdorsal/postkutsche/)
+
 # Install
 ```
 npm i postkutsche
+```
+# Basic use
+```js
+(async () => {
+    //import the process.env variables from the .env file in which you should store them
+    //you can install dotenv with `npm i dotenv --save-dev` or `yarn add dotenv --dev`
+    require('dotenv').config();
+
+    //import the Postkutsche class
+    const {
+        Postkutsche
+    } = require("./index.js");
+
+    //create a new instance of Postkutsche providing it with the necessary api keys
+    const pk = new Postkutsche({
+        mailcow: {
+            url: process.env.MAILCOW_API_URL,
+            apikey: process.env.MAILCOW_API_KEY
+        },
+        powerdns: {
+            url: process.env.PDNS_API_URL,
+            apikey: process.env.PDNS_API_KEY
+        }
+    });
+
+    const info={
+        nameserver: ['ns1.domain.tld', 'ns2.domain.tld', 'ns3.domain.tld'],
+        hostmasterEmail: 'hostmaster@domain.tld',
+        dmarcMail: 'postmaster@domain.tld', 
+        mailDomain: 'domain.tld',
+        mailServerHostname: 'mail.domain.tld',
+        defaultMailbox: {
+            local_part: `max.mustermensch`,
+            name: `Max Mustermensch`
+        },
+        //NEEDED FOR MAILSERVER DOMAIN SETUP
+        mailServerIp: '2a00:1450:4016:801::2003', //Your IPv6 address WITHOUT brackets([]) 
+        mailServerLegacyIp: '127.0.0.1',//Your IPv4 address
+        addLetsEncryptCAA: true,
+        caaReportMail: 'caa.report@domain.tld' //can be set if you want to get reports on malicious cert issues
+    }
+
+    //use a function from the Postkutsche class
+    //adds the mail records for a domain and creates the domain on mailcow
+    await pk.addMailDomain(info);
+})();
 ```
 
 
@@ -10,6 +62,6 @@ Feel free to contact me via [xl9jthv_7bvgakv9o9wg0jabn2ylm91xxrzzgt0e@y.gy](mail
 ## Links
 [NPM](https://www.npmjs.com/package/postkutsche)
 
-[Documentation](https://firstdorsal.eu/doc/postkutsche/)
+[Documentation](https://doc.y.gy/postkutsche/)
 
 [Code](https://git.y.gy/firstdorsal/postkutsche)
--- index.raw.js
+++ index.raw.js
@@ -46,6 +46,7 @@ const {
 * @prop {Object} defaultMailbox
 * @prop {String} defaultMailbox.local_part local part of you mailbox (the part before the @ not including the @)
 * @prop {String} defaultMailbox.name the name of mailbox
+* @prop {String} [defaultMailbox.password=RANDOM] will generate a random password for your mailbox if omitted
 * @prop {String} mailServerIp IPv6 address of your mailserver
 * @prop {String} mailServerLegacyIp IPv4 address of your mailserver
 * @prop {Boolean=} addLetsEncryptCAA enable this option if you are ONLY using letsencrypt certs
@@ -97,7 +98,24 @@ const {
         }
     });
 
-    //use a function from the pk class
+    const info={
+        nameserver: ['ns1.domain.tld', 'ns2.domain.tld', 'ns3.domain.tld'],
+        hostmasterEmail: 'hostmaster@domain.tld',
+        dmarcMail: 'postmaster@domain.tld', 
+        mailDomain: 'domain.tld',
+        mailServerHostname: 'mail.domain.tld',
+        defaultMailbox: {
+            local_part: `max.mustermensch`,
+            name: `Max Mustermensch`
+        },
+        //NEEDED FOR MAILSERVER DOMAIN SETUP
+        mailServerIp: '2a00:1450:4016:801::2003', //Your IPv6 address WITHOUT brackets([]) 
+        mailServerLegacyIp: '127.0.0.1',//Your IPv4 address
+        addLetsEncryptCAA: true,
+        caaReportMail: 'caa.report@domain.tld' //can be set if you want to get reports on malicious cert issues
+    }
+
+    //use a function from the Postkutsche class
     //adds the mail records for a domain and creates the domain on mailcow
     await pk.addMailDomain(info);
 })();
@@ -178,7 +196,7 @@ module.exports.Postkutsche = class {
         const answers = await Promise.all(questions.map((e) => {
             e.answer = exec(`
         echo | 
-        ${openssl_path} s_client -servername ${info.mailServerHostname} -connect ${info.mailServerIp?info.mailServerIp:info.mailServerLegacyIp}:${e.port} ${e.starttls?`-starttls `+e.starttls.type:``} 2>/dev/null | 
+        ${info.openssl_path} s_client -servername ${info.mailServerHostname} -connect ${info.mailServerIp?info.mailServerIp:info.mailServerLegacyIp}:${e.port} ${e.starttls?`-starttls `+e.starttls.type:``} 2>/dev/null | 
         openssl x509 -pubkey -noout | 
         openssl pkey -pubin -outform DER | 
         openssl sha256 |
@@ -306,7 +324,9 @@ module.exports.Postkutsche = class {
      *      - DNSSEC (if domain wasn't present)
      *      - Create record on mailServerDomain(if not the same as mailDomain) to allow dmarc mails to sent to this domain
      * @param {Info} info {@link DOC_URL/global.html#Info Info} object with the necessary information to create a mail domain on mailcow and the necessary records on powerdns
+     * @param {Boolean} [log=true] you can disable logging by setting this to false
      * @async
+     * @returns {Boolean} true on success
      * @example
         await pk.addMailDomain({
             nameserver: ['ns1.domain.tld', 'ns2.domain.tld', 'ns3.domain.tld'],
@@ -316,27 +336,49 @@ module.exports.Postkutsche = class {
             mailServerHostname: 'mail.domain.tld',
             defaultMailbox: {
                 local_part: `max.mustermensch`,
-                name: `Max Mustermensch`
+                name: `Max Mustermensch`,
+                password:`set some good password here` //can be omitted
             }
         });
      */
-    addMailDomain = async (info) => {
+    addMailDomain = async (info, log = true) => {
         info.domain = info.mailDomain;
+        if (log) {
+            console.log(`Adding zone ${info.mailDomain} to PowerDns if it doesn't exist`);
+            console.log(`Adding domain ${info.mailDomain} to Mailcow if it doesn't exist`);
+            console.log(`Adding DKIM key for ${info.mailDomain} to Mailcow if it doesn't exist`);
+        }
         const add = await Promise.all([this.pdns.createAndSetupZone(info), this.mcc.addDomain(info.mailDomain), this.mcc.addAndGetDKIM(info.mailDomain)]);
+
+        console.log(`You have to add the following key to your domain at your registrar (where you bought the domain)`);
+        console.log(add[0]);
+
+        if (log) {
+            console.log(`Generating mail domain records for ${info.mailDomain}`);
+        }
         const records = this.genMailDomainRecords(info);
+
         records.push({
             name: `dkim._domainkey.${info.mailDomain}.`,
             type: 'TXT',
             content: [`"${add[2].dkim_txt}"`]
         });
+
         if (info.dmarcMail && info.mailDomain !== info.dmarcMail.match(secondLevelRegex)[0]) {
+            if (log) {
+                console.log(`Adding record to DMARC mail domain ${info.dmarcMail.match(secondLevelRegex)[0]} to allow the sending of ${info.mailDomain} DMARC reports there`);
+            }
             this.pdns.setHomogeneousRecords([{
                 name: `${info.mailDomain}._report._dmarc.${info.dmarcMail.match(secondLevelRegex)[0]}`,
                 type: 'TXT',
                 content: [`"v=DMARC1"`]
             }]).catch(e => console.log(e))
         }
-        await Promise.all([
+        if (log) {
+            console.log(`Adding generated records to PowerDns server`);
+            console.log(`Adding mailbox to Mailcow server`);
+        }
+        const b = await Promise.all([
             this.pdns.setHomogeneousRecords(records).catch(e => console.log(e)),
             this.mcc.addMailbox({
                 domain: info.mailDomain,
@@ -345,28 +387,58 @@ module.exports.Postkutsche = class {
                 password: info.defaultMailbox.password ? info.defaultMailbox.password : undefined
             })
         ]);
-        this.mcc.addAlias(`@${info.mailDomain}`, `${info.defaultMailbox.local_part}@${info.mailDomain}`)
+
+        if (log) {
+            console.log(`Adding catchall alias to ${info.mailDomain}`);
+        }
+        this.mcc.addAlias(`@${info.mailDomain}`, `${info.defaultMailbox.local_part}@${info.mailDomain}`);
+        if (!info.defaultMailbox.password) {
+            console.log(`The password for your mailbox is`);
+            console.log(b[1].password);
+        }
+        if (log) {
+            console.log(`Done adding domain ${info.mailDomain}`);
+        }
+        console.log(`Don't forget to add the DNSSEC domainkey above to your registrar.`);
+        return true;
+
     }
 
     /**
      * This will add:
      *  - PowerDns: 
      *      - Domain for the mailserver hostname (if not present)
-     *      - DNSSEC (if domain wasn't present)
-     *      - TLSA records for the domain (for the creation of the tlsa records you need to have openssl installed. you can specify the path if it can't be found globally as 'openssl')
+     *      - DNSSEC (will replace old dnssec if present)
+     *      - TLSA records for the domain (for the creation of the tlsa records you need to have openssl installed. you can specify the path, if it can't be found globally as 'openssl')
      *      - Records for the mailserver
      * @param {Info} info {@link DOC_URL/global.html#Info Info} object with the necessary information to create the relevant records for the mailserver
+     * @param {Boolean} [log=true] you can disable logging by setting this to false
      * @async
+     * @returns {Boolean} true on success
      * @example
      */
-    addMailServerDnsRecords = async (info) => {
+    addMailServerDnsRecords = async (info, log = true) => {
         info.domain = info.mailServerHostname
-        await this.pdns.createAndSetupZone(info).catch(e => {
+        if (log) {
+            console.log(`Adding zone ${info.mailServerHostname} to PowerDns if it doesn't exist`);
+        }
+        const z = await this.pdns.createAndSetupZone(info).catch(e => {
             console.log(e);
         });
+
+        console.log(`You have to add the following key to your domain at your registrar (where you bought the domain)`);
+        console.log(z);
+
+        if (log) {
+            console.log(`Creating TLSA records for your mail server domain.`);
+            console.log(`Info: You need to have openssl installed for this to work`);
+            console.log(`Info: If openssl is not accessible throug the global command 'openssl' you can specify the path by adding it to the passed info object with the key 'openssl_path' for more information look here DOC_URL/global.html#Info`);
+        }
         const records = (await this.getTLSA(info)).map((e) => {
             return e.dns
         });
+        if (log) console.log(`Adding mail server ip records`);
+
         if (info.mailServerLegacyIp) {
             records.push({
                 name: info.mailServerHostname,
@@ -382,6 +454,8 @@ module.exports.Postkutsche = class {
             });
         }
         if (info.addLetsEncryptCAA) {
+            if (log) console.log(`Adding mail server caa records`);
+
             const content = [`0 issue "digicert.com"`, `0 issue "letsencrypt.org"`];
             info.caaReportMail ? content.push(`0 iodef "mailto:${info.caaReportMail}"`) : '';
             records.push({
@@ -390,9 +464,16 @@ module.exports.Postkutsche = class {
                 content: content
             });
         }
+        if (log) console.log(`Setting all records on the PowerDns server`);
+
         this.pdns.setHomogeneousRecords(records).catch(e => {
             console.log(e);
         });
+        if (log) console.log(`Finished adding records for the mail server ${info.mailServerHostname}`);
+        console.log(`Don't forget to add the DNSSEC domainkey above to your registrar.`);
+
+        return true;
+
     }
 
     /**
